e6e24a959dfeeaf733a74e348845173dd282d692
gretor0231 <gretor0231@gmail.com>
2024-07-17 13:56:51 +0800

    main: 1.added player.rs.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use rusty_audio::Audio;
 use invaderstest::{frame, render};
-use invaderstest::frame::new_frame;
+use invaderstest::frame::{Drawable, new_frame};
+use invaderstest::player::Player;
 use invaderstest::render::render;
 
 fn main() -> Result<(),Box<dyn Error>> {
@@ -47,15 +48,18 @@ fn main() -> Result<(),Box<dyn Error>> {
 
 
     // Game Loop
+    let mut player = Player::new();
     'gameloop:loop{
 
         // Per frame init
-        let curr_frame = new_frame();
+        let mut curr_frame = new_frame();
 
         // Input
         while event::poll(Duration::default())?{
             if let Event::Key(key_event) = event::read()?{
                 match key_event.code{
+                    KeyCode::Left=> player.move_left(),
+                    KeyCode::Right=> player.move_right(),
                     KeyCode::Esc| KeyCode::Char('q')=>{
                         audio.play("lose");
                         break 'gameloop;
@@ -65,6 +69,7 @@ fn main() -> Result<(),Box<dyn Error>> {
             }
         }
         // Draw & render
+        player.draw(&mut curr_frame);
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
