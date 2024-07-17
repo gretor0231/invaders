4f6f6c70225e328e4c509190aa713dbb9c615d40
gretor0231 <gretor0231@gmail.com>
2024-07-17 10:17:32 +0800

    main: init the project.

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+
+
+
+
+    //terminal
+    // let mut sudout = io::sudout();
+    // terminal::enable_raw_mode();
+    // sudout.execute(EnterAlternateScreen)?;
+
+
+
+
+}
