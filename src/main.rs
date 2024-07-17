37d8dc4d230a12db77985e3ad5503b7300efcccc
gretor0231 <gretor0231@gmail.com>
2024-07-17 13:23:54 +0800

    main: 1.added render sections.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use std::error::Error;
-use std::io;
+use std::{io, thread};
 use std::io::stdout;
+use std::sync::mpsc;
 use std::time::Duration;
 use crossterm::{event, ExecutableCommand, terminal};
 use crossterm::cursor::{Hide, Show};
 use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use rusty_audio::Audio;
-
+use invaderstest::{frame, render};
+use invaderstest::frame::new_frame;
+use invaderstest::render::render;
 
 fn main() -> Result<(),Box<dyn Error>> {
 
@@ -26,10 +29,30 @@ fn main() -> Result<(),Box<dyn Error>> {
     stdout().execute(Hide);
 
 
+    // Render loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move||{
+       let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop{
+            let curr_frame = match render_rx.recv(){
+                Ok(x)=>x,
+                Err(x)=>break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+
     // Game Loop
-
-
     'gameloop:loop{
+
+        // Per frame init
+        let curr_frame = new_frame();
+
+        // Input
         while event::poll(Duration::default())?{
             if let Event::Key(key_event) = event::read()?{
                 match key_event.code{
@@ -41,22 +64,19 @@ fn main() -> Result<(),Box<dyn Error>> {
                 }
             }
         }
+        // Draw & render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
     }
 
 
-
-
-
-
     //Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
+
     audio.wait();
     stdout().execute(Show)?;
     stdout().execute(LeaveAlternateScreen)?;
     terminal::disable_raw_mode()?;
     Ok(())
-
-
-
-
-
 }
