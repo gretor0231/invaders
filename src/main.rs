72c7594438995f9d18a54312c2662698728a9f3c
gretor0231 <gretor0231@gmail.com>
2024-07-17 11:01:14 +0800

    main: run audio sounds.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-fn main() {
+use std::error::Error;
+use rusty_audio::Audio;
+
+fn main() -> Result<(),Box<dyn Error>> {
+
+    let mut audio = Audio::new();
+    audio.add("explode", "explode.wav");
+    audio.add("lose", "lose.wav");
+    audio.add("pew", "pew.wav");
+    audio.add("startup", "startup.wav");
+    audio.add("win", "win.wav");
+    audio.play("startup");
+
     println!("Hello, world!");
 
-
-
+    //cleanup
+    audio.wait();
+    Ok(())
 
     //terminal
     // let mut sudout = io::sudout();
