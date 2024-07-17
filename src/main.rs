f2310e539bd24bdae5220f7fc65cc4bdcb3cc968
gretor0231 <gretor0231@gmail.com>
2024-07-17 12:47:39 +0800

    main: 1.added looping logic.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use std::error::Error;
+use std::io;
+use std::io::stdout;
+use std::time::Duration;
+use crossterm::{event, ExecutableCommand, terminal};
+use crossterm::cursor::{Hide, Show};
+use crossterm::event::{Event, KeyCode};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use rusty_audio::Audio;
+
 
 fn main() -> Result<(),Box<dyn Error>> {
 
@@ -11,16 +19,42 @@ fn main() -> Result<(),Box<dyn Error>> {
     audio.add("win", "win.wav");
     audio.play("startup");
 
-    println!("Hello, world!");
+    // terminal
+    let mut sudout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout().execute(EnterAlternateScreen)?;
+    stdout().execute(Hide);
 
-    //cleanup
+
+    // Game Loop
+
+
+    'gameloop:loop{
+        while event::poll(Duration::default())?{
+            if let Event::Key(key_event) = event::read()?{
+                match key_event.code{
+                    KeyCode::Esc| KeyCode::Char('q')=>{
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+    //Cleanup
     audio.wait();
+    stdout().execute(Show)?;
+    stdout().execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 
-    //terminal
-    // let mut sudout = io::sudout();
-    // terminal::enable_raw_mode();
-    // sudout.execute(EnterAlternateScreen)?;
 
 
 
